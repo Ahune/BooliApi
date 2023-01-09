fd9601ea68c5ee38feed331020f7286c3edbaf77
Alexander <Alexander.mounzer@gmail.com>
2023-01-09 16:21:40 +0100

    first commit

---
+++ BooliAPI/BooliObject.cs
@@ -0,0 +1,76 @@
+﻿namespace BooliAPI
+{
+    public class BooliObject
+    {
+        public class Rootobject
+        {
+            public int totalCount { get; set; }
+            public int count { get; set; }
+            public Sold[]? sold { get; set; }
+            public int limit { get; set; }
+            public int offset { get; set; }
+            public Searchparams? searchParams { get; set; }
+        }
+
+        public class Searchparams
+        {
+            public int areaId { get; set; }
+        }
+        public class Sold
+        {
+            public Source? source { get; set; }
+            public int rooms { get; set; }
+            public int constructionYear { get; set; }
+            public string? soldDate { get; set; }
+            public Location? location { get; set; }
+            public int additionalArea { get; set; }
+            public int rent { get; set; }
+            public int livingArea { get; set; }
+            public string? soldPriceSource { get; set; }
+            public int listPrice { get; set; }
+            public int soldPrice { get; set; }
+            public int booliId { get; set; }
+            public int floor { get; set; }
+            public string? objectType { get; set; }
+            public string? published { get; set; }
+            public string? apartmentNumber { get; set; }
+            public string? url { get; set; }
+        }
+        public class Source
+        {
+            public int id { get; set; }
+            public string? url { get; set; }
+            public string? type { get; set; }
+            public string? name { get; set; }
+        }
+        public class Location
+        {
+            public Address? address { get; set; }
+            public Position? position { get; set; }
+            public Region? region { get; set; }
+            public Distance? distance { get; set; }
+            public string[]? namedAreas { get; set; }
+        }
+
+        public class Address
+        {
+            public string? city { get; set; }
+            public string? streetAddress { get; set; }
+        }
+        public class Position
+        {
+            public float latitude { get; set; }
+            public float longitude { get; set; }
+        }
+        public class Region
+        {
+            public string? municipalityName { get; set; }
+            public string? countyName { get; set; }
+        }
+        public class Distance
+        {
+            public int ocean { get; set; }
+        }
+    }
+}
+
---
+++ BooliAPI/Program.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<PropertiesSoldDb>(opt => opt.UseInMemoryDatabase("PropertiesSoldList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World Let calculate some prices in the real estate market! Start by using Boolis API to return a JSON of sold element.");
+
+app.MapGet("/mylist", async (PropertiesSoldDb db) =>
+    await db.SoldProperties.ToListAsync());
+
+app.MapGet("/mylist/{id}", async (int id, PropertiesSoldDb db) =>
+    await db.SoldProperties.FindAsync(id)
+        is SoldProperty sell
+            ? Results.Ok(sell)
+            : Results.NotFound());
+
+app.MapGet("/avgprice", async (PropertiesSoldDb db) =>
+{
+    var objects = await db.SoldProperties.ToListAsync();
+
+    var avgPricePerSquareMeter = CalcAvgPrice(objects);
+    return Results.Ok(avgPricePerSquareMeter);
+});
+
+app.MapPost("/solditems", async (SoldProperty sells, PropertiesSoldDb db) =>
+{
+    if (sells is null)
+    {
+        return Results.NotFound();
+    }
+
+    db.SoldProperties.Add(sells);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/Solditems/{sells.Id}", sells);
+
+});
+
+app.MapPost("/multipleAdd", async (List<SoldProperty> sells, PropertiesSoldDb db) =>
+{
+    if (sells is null || sells.Count <= 0)
+    {
+        return Results.NotFound();
+    }
+
+    foreach (var item in sells)
+    {
+        db.SoldProperties.Add(item);
+
+    }
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/Solditems/{sells.Count}", sells);
+
+});
+
+app.MapPut("/Solditems/{id}", async (int id, SoldProperty inputSold, PropertiesSoldDb db) =>
+{
+    var sold = await db.SoldProperties.FindAsync(id);
+
+    if (sold is null) return Results.NotFound();
+
+    sold.Rooms = inputSold.Rooms;
+    sold.ConstructionYear = inputSold.ConstructionYear;
+    sold.SoldDate = inputSold.SoldDate;
+    sold.AdditionalArea = inputSold.AdditionalArea;
+    sold.Rent = inputSold.Rent;
+    sold.LivingArea = inputSold.LivingArea;
+    sold.SoldPriceSource = inputSold.SoldPriceSource;
+    sold.ListPrice = inputSold.ListPrice;
+    sold.SoldPrice = inputSold.SoldPrice;
+    sold.BooliId = inputSold.BooliId;
+    sold.Floor = inputSold.Floor;
+    sold.ObjectType = inputSold.ObjectType;
+    sold.Published = inputSold.Published;
+    sold.ApartmentNumber = inputSold.ApartmentNumber;
+    sold.Url = inputSold.Url;
+
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+
+});
+
+app.MapDelete("/solditems/{id}", async (int id, PropertiesSoldDb db) =>
+{
+    if (await db.SoldProperties.FindAsync(id) is SoldProperty soldItem)
+    {
+        db.Remove(soldItem);
+        await db.SaveChangesAsync();
+        return Results.Ok(soldItem);
+    }
+    return Results.NotFound();
+});
+
+app.Run();
+
+
+//Better on the client side
+float CalcAvgPrice(List<SoldProperty> objects)
+{
+    float avgPricePerSquareMeter = 0f;
+    float sqrmeter = 0f;
+    float sellingPrice = 0f;
+    float pricePerSqr = 0f;
+    int numberOfObjects = objects.Count;
+    List<float> pricePerSqrList = new List<float>();
+
+    foreach (var item in objects)
+    {
+        sellingPrice = (float)item.SoldPrice;
+        sqrmeter = (float)item.LivingArea;
+        pricePerSqr = sellingPrice / sqrmeter;
+        pricePerSqrList.Add(pricePerSqr);
+    }
+
+    avgPricePerSquareMeter = (pricePerSqrList.Sum() / pricePerSqrList.Count);
+
+
+    return MathF.Round(avgPricePerSquareMeter);
+} 
+
+class PropertiesSoldDb : DbContext
+{
+    public PropertiesSoldDb(DbContextOptions<PropertiesSoldDb> options)
+        : base(options) { }
+
+    public DbSet<SoldProperty> SoldProperties => Set<SoldProperty>();
+}
+
+class SoldProperty
+{
+    public int Id { get; set; }
+    public int Rooms { get; set; }
+    public int ConstructionYear { get; set; }
+    public string? SoldDate { get; set; }
+    public int AdditionalArea { get; set; }
+    public int Rent { get; set; }
+    public int LivingArea { get; set; }
+    public string? SoldPriceSource { get; set; }
+    public float ListPrice { get; set; }
+    public int SoldPrice { get; set; }
+    public int BooliId { get; set; }
+    public int Floor { get; set; }
+    public string? ObjectType { get; set; }
+    public string? Published { get; set; }
+    public string? ApartmentNumber { get; set; }
+    public string? Url { get; set; }
+}
